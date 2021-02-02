11fea67519102a3e48a01e04aa567a61011e3ab3
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 17:37:41 -0500

    Update DocumentHeadingRepository.java

--- backend/src/main/java/com/search/sdasearch/dao/DocumentHeadingRepository.java
+++ backend/src/main/java/com/search/sdasearch/dao/DocumentHeadingRepository.java
@@ -7,6 +7,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**************************************/
+/*                                    */
+/*  Program: SDA-Search               */
+/*  Author: Paul Trudel               */
+/*                                    */
+/*  JPA repository for the            */
+/*  DocumentHeading entity            */
+/*                                    */
+/**************************************/
+
 @CrossOrigin("http://localhost:4200")
 public interface DocumentHeadingRepository extends JpaRepository<DocumentHeading, Long> {
 
