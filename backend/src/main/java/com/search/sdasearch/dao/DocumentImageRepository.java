3cc41954bc45abd1950a7b8b9cfa9b02fefd9024
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 17:39:28 -0500

    Update DocumentImageRepository.java

--- backend/src/main/java/com/search/sdasearch/dao/DocumentImageRepository.java
+++ backend/src/main/java/com/search/sdasearch/dao/DocumentImageRepository.java
@@ -7,8 +7,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**************************************/
+/*                                    */
+/*  Program: SDA-Search               */
+/*  Author: Paul Trudel               */
+/*                                    */
+/*  JPA repository for the            */
+/*  DocumentImage entity              */
+/*                                    */
+/**************************************/
+
 @CrossOrigin("http://localhost:4200")
 public interface DocumentImageRepository extends JpaRepository<DocumentImage, Long> {
 
+    // Return page of DocumentImages corresponding to the given document ID
     Page<DocumentImage> findByDocumentId(@RequestParam("id") Long id, Pageable pageable);
 }
