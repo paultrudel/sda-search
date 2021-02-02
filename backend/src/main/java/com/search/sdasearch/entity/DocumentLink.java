f7395f27471b2b5dd31601ac68b06f56962f18b9
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 18:06:49 -0500

    Update DocumentLink.java

--- backend/src/main/java/com/search/sdasearch/entity/DocumentLink.java
+++ backend/src/main/java/com/search/sdasearch/entity/DocumentLink.java
@@ -4,20 +4,30 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "document_link")
-@Data
+/**************************************/
+/*                                    */
+/*  Program: SDA-Search               */
+/*  Author: Paul Trudel               */
+/*                                    */
+/*  Class which defines the           */
+/*  DocumentLink entity               */
+/*                                    */
+/**************************************/
+
+@Entity // Anotate class as JPA entity
+@Table(name = "document_link") // Anotate table corresponding to the entity
+@Data // Lombok annotation to automatically generate boilderplate code
 public class DocumentLink {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private Long id; // Entity primary key
 
     @Column(name = "text")
     private String text;
 
     @ManyToOne
     @JoinColumn(name = "document_id")
-    private SDADocument document;
+    private SDADocument document; // Define Many-to-One relationship between DocumentLink and SDADocument entities
 }
