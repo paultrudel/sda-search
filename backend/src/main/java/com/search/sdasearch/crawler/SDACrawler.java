2aab67c5e7808f912b992e3a50aa9d7ba76301bc
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 17:36:29 -0500

    Update SDACrawler.java

--- backend/src/main/java/com/search/sdasearch/crawler/SDACrawler.java
+++ backend/src/main/java/com/search/sdasearch/crawler/SDACrawler.java
@@ -5,19 +5,30 @@ import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.url.WebURL;
 
+/**************************************/
+/*                                    */
+/*  Program: SDA-Search               */
+/*  Author: Paul Trudel               */
+/*                                    */
+/*  User defined web crawler          */
+/*                                    */
+/**************************************/
+
 public class SDACrawler extends WebCrawler {
 
-    private SDADocumentHandler documentHandler;
+    private SDADocumentHandler documentHandler; // Class response for handling the parsing of crawled documents
 
     public SDACrawler() {
         documentHandler = new SDADocumentHandler();
     }
 
+    // Determine whether the next page in the queue should be visited
     @Override
     public boolean shouldVisit(Page refferingPage, WebURL url) {
         return true;
     }
 
+    // Pass the visited page of to the DocumentHandler for parsing
     @Override
     public void visit(Page page) {
         logger.info("========== Visiting Page {} ==========", page.getWebURL());
