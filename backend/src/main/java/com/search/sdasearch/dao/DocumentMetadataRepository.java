8093fa069c28938fcc4bb3b1c03ab0b9f2cc66d8
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 18:01:02 -0500

    Update DocumentMetadataRepository.java

--- backend/src/main/java/com/search/sdasearch/dao/DocumentMetadataRepository.java
+++ backend/src/main/java/com/search/sdasearch/dao/DocumentMetadataRepository.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 /*  Program: SDA-Search               */
 /*  Author: Paul Trudel               */
 /*                                    */
-/*  JPA repository for the            */
-/*  DocumentMetadata entity           */
+/*  Spring Data JPA repository for    */
+/*  the DocumentMetadata entity       */
 /*                                    */
 /**************************************/
 
