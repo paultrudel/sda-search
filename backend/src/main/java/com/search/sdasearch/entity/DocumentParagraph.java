ad0a7df4723707a8102c87e6de4a7b9395394d69
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 18:11:24 -0500

    Update DocumentParagraph.java

--- backend/src/main/java/com/search/sdasearch/entity/DocumentParagraph.java
+++ backend/src/main/java/com/search/sdasearch/entity/DocumentParagraph.java
@@ -4,20 +4,30 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "document_paragraph")
-@Data
+/**************************************/
+/*                                    */
+/*  Program: SDA-Search               */
+/*  Author: Paul Trudel               */
+/*                                    */
+/*  Class which defines the           */
+/*  DocumentParagraph entity          */
+/*                                    */
+/**************************************/
+
+@Entity // Annotate class as JPA entity
+@Table(name = "document_paragraph") // Annotate table corresponding to entity
+@Data // Lombok annotation to automatically generate boilerplate code
 public class DocumentParagraph {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private Long id; // Entity primary key
 
     @Column(name = "text")
     private String text;
 
     @ManyToOne
     @JoinColumn(name = "document_id")
-    private SDADocument document;
+    private SDADocument document; // Define Many-to-One relationship between DocumentParagraph and SDADocument entities
 }
