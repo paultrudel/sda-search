607a7427a8326948fbc7410bd89565019962fe0c
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 17:58:37 -0500

    Update DocumentHeading.java

--- backend/src/main/java/com/search/sdasearch/entity/DocumentHeading.java
+++ backend/src/main/java/com/search/sdasearch/entity/DocumentHeading.java
@@ -4,21 +4,31 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "document_heading")
-@Data
+/**************************************/
+/*                                    */
+/*  Program: SDA-Search               */
+/*  Author: Paul Trudel               */
+/*                                    */
+/*  Class which defines the           */
+/*  DocumentHeading entity            */
+/*                                    */
+/**************************************/
+
+@Entity // Annotate class JPA entity
+@Table(name = "document_heading") // Annotate the table corresponding to the entity
+@Data // Lombok annotation to automatically generate boilerplate code
 public class DocumentHeading {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private Long id; // Entity primary key
 
     @Column(name = "text")
     private String text;
 
     @ManyToOne
     @JoinColumn(name = "document_id")
-    private SDADocument document;
+    private SDADocument document; // Define Many-to-One relationship between DocumentHeading and SDADocument entities
 
 }
