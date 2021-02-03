a60803bb48bae28f4484828699cb5f15fa08508b
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 20:17:37 -0500

    Update SDARestController.java

--- backend/src/main/java/com/search/sdasearch/rest/SDARestController.java
+++ backend/src/main/java/com/search/sdasearch/rest/SDARestController.java
@@ -8,6 +8,17 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+/**************************************/
+/*                                    */
+/*  Program: SDA-Search               */
+/*  Author: Paul Trudel               */
+/*                                    */
+/*  Application controller to expose  */
+/*  additional REST enpoints and      */
+/*  handle requests                   */
+/*                                    */
+/**************************************/
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin("http://localhost:4200")
@@ -15,17 +26,19 @@ public class SDARestController {
 
     private SDADocumentService documentService;
 
+    // Inject the document service using constructor injection
     @Autowired
     public SDARestController(
             SDADocumentService documentService) {
         this.documentService = documentService;
     }
 
+    // Expose endpoint at /api/search which accepts GET requests and returns a Page of SDADocuments
     @GetMapping("/search")
     public Page<SDADocument> searchDocuments(@RequestParam("query") String query,
                                              @RequestParam("page") int page,
                                              @RequestParam("size") int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        return documentService.searchDocuments(query, pageable);
+        Pageable pageable = PageRequest.of(page, size); // Create new pageable with using the request parameters
+        return documentService.searchDocuments(query, pageable); // Use the document service to fetch documents using the user provided query
     }
 }
