4dfe29d0805e29acb33aa0d471cfa77813fcf04f
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 20:33:53 -0500

    Update Paragraph.java

--- backend/src/main/java/com/search/sdasearch/summarizer/Paragraph.java
+++ backend/src/main/java/com/search/sdasearch/summarizer/Paragraph.java
@@ -5,16 +5,27 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+/**************************************/
+/*                                    */
+/*  Program: SDA-Search               */
+/*  Author: Paul Trudel               */
+/*                                    */
+/*  Representation of a paragraph in  */
+/*  a body of text                    */
+/*                                    */
+/**************************************/
+
+@Data // Lombok annotation to generate boilerplate code
 public class Paragraph {
 
-    private int paragraphNumber;
-    private List<Sentence> sentences;
+    private int paragraphNumber; // The paragraph number in the body of text
+    private List<Sentence> sentences; // The sentences contained in the paragraph
 
     public Paragraph(int paragraphNumber) {
         this.paragraphNumber = paragraphNumber;
     }
 
+    // Add a sentence to the paragraph
     public void addSentence(Sentence sentence) {
         if(sentences == null) {
             sentences = new ArrayList<>();
