959bfb0df7e6fd9c1d43cafad6f20bdbf83a3fc3
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 20:31:39 -0500

    Update SDADocumentServiceImpl.java

--- backend/src/main/java/com/search/sdasearch/service/SDADocumentServiceImpl.java
+++ backend/src/main/java/com/search/sdasearch/service/SDADocumentServiceImpl.java
@@ -26,12 +26,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+/**************************************/
+/*                                    */
+/*  Program: SDA-Search               */
+/*  Author: Paul Trudel               */
+/*                                    */
+/*  Implementation of the             */
+/*  SDADocumentService                */
+/*                                    */
+/**************************************/
+
+@Service // Annotate class so that it can be injected
 public class SDADocumentServiceImpl implements SDADocumentService {
 
     private static final Logger logger = LoggerFactory.getLogger(
             SDADocumentServiceImpl.class);
 
+    // Field names contained in the document index
     public static final String ID = "id";
     public static final String TITLE = "title";
     public static final String CONTENT = "content";
@@ -40,64 +51,73 @@ public class SDADocumentServiceImpl implements SDADocumentService {
 
     private SDADocumentRepository documentRepository;
 
+    // Inject the document repository using constructor injection
     @Autowired
     public SDADocumentServiceImpl(SDADocumentRepository documentRepository) {
         this.documentRepository = documentRepository;
     }
 
+    // Search the index for relevant documents using the user provided query
     @Override
     public Page<SDADocument> searchDocuments(String queryString, Pageable pageable) {
         logger.info("========= Starting search for query: {} ========", queryString);
+        // If the query is an empty string then return all of the documents
         if(queryString.isEmpty()) {
             return documentRepository.findAll(pageable);
         }
         try {
             IndexReader reader = DirectoryReader.open(FSDirectory.open(
                     new File(SDASetup.INDEX).toPath()
-            ));
-            IndexSearcher searcher = new IndexSearcher(reader);
-            Analyzer analyzer = new StandardAnalyzer();
+            )); // Open an index reader at the directory containing the document index
+            IndexSearcher searcher = new IndexSearcher(reader); // Initialize a index searcher using the reader
+            Analyzer analyzer = new StandardAnalyzer(); // Create a new document analyzer
 
+            // Create a query to search the title field in the indexed documents
             QueryParser titleParser = new QueryParser(TITLE, analyzer);
             Query titleQuery = titleParser.parse(queryString);
 
+            // Create a query to search the content field in the indexed documents
             QueryParser contentParser = new QueryParser(CONTENT, analyzer);
             Query contentQuery = contentParser.parse(queryString);
 
-            //Query boostQuery = FeatureField.newSaturationQuery(FEATURES, PAGERANK);
+            // Create a query to boost documents scores using the stored PageRank scores
+            Query boostQuery = FeatureField.newSaturationQuery(FEATURES, PAGERANK);
 
+            // Create the final query which combines all of the previous queries
             Query finalQuery = new BooleanQuery.Builder()
                     .add(titleQuery, BooleanClause.Occur.MUST)
                     .add(contentQuery, BooleanClause.Occur.MUST)
-                    //.add(boostQuery, BooleanClause.Occur.SHOULD)
+                    .add(boostQuery, BooleanClause.Occur.SHOULD)
                     .build();
-
-            TopDocs topDocs = searcher.search(finalQuery, 100);
-            ScoreDoc[] hits = topDocs.scoreDocs;
+            
+            TopDocs topDocs = searcher.search(finalQuery, 100); // Search the index and return the top 100 documents
+            ScoreDoc[] hits = topDocs.scoreDocs; // Get the documents that were returned
             logger.info("Search returned {} documents", hits.length);
-            return documentListToPage(getDocuments(searcher, hits), pageable);
+            return documentListToPage(getDocuments(searcher, hits), pageable); // Return the list of documents as a page
         } catch(Exception e) {
             e.printStackTrace();
         }
         return null;
     }
 
+    // Map the indexed documents to their corresponding SDADocument and return the list
     private List<SDADocument> getDocuments(IndexSearcher searcher, ScoreDoc[] hits) {
         List<SDADocument> documents = new ArrayList<>();
 
+        // Loop over all over the returned documents
         for(ScoreDoc hit: hits) {
             Document indexDocument;
             try {
-                indexDocument = searcher.doc(hit.doc);
+                indexDocument = searcher.doc(hit.doc); // Get the document from the index
                 logger.info("Getting document from index with ID {}", indexDocument.get(ID));
-                Long id = Long.parseLong(indexDocument.get(ID));
+                Long id = Long.parseLong(indexDocument.get(ID)); // Get the value of the ID field from the document
                 if(id != null) {
                     logger.info("Getting document from DB with ID {}", id);
-                    Optional<SDADocument> result = documentRepository.findById(id);
+                    Optional<SDADocument> result = documentRepository.findById(id); // Use the ID to get the corresponding SDADocument
                     SDADocument sdaDocument = null;
                     if(result.isPresent()) {
-                        sdaDocument = result.get();
-                        sdaDocument.setQueryScore(hit.score);
+                        sdaDocument = result.get(); // Get the SDaDocument
+                        sdaDocument.setQueryScore(hit.score); // Set the query score assigned to the document by Lucene
                         documents.add(sdaDocument);
                     }
                 }
@@ -106,17 +126,21 @@ public class SDADocumentServiceImpl implements SDADocumentService {
             }
         }
 
-        return documents;
+        return documents; // Return the list of SDADocuments
     }
 
+    // Convert the list of SDADocuments to a page
     private Page<SDADocument> documentListToPage(List<SDADocument> documents,
                                                  Pageable pageable) {
         logger.info("Converting returned document list to page");
-        int start = (int) pageable.getOffset();
-        int end = (int) (Math.min((start + pageable.getPageSize()), documents.size()));
+        int start = (int) pageable.getOffset(); // Get the start index
+        int end = (int) (Math.min((start + pageable.getPageSize()), documents.size())); // Compute the end index
+        
+        // Create a page of SDADocuments
         Page<SDADocument> documentsPage = new PageImpl<>(
                 documents.subList(start, end), pageable, documents.size()
         );
-        return documentsPage;
+        
+        return documentsPage; // Return the page of SDADocuments
     }
 }
