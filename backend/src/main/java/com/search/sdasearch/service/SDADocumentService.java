c0413acefabde2178928bb152c109c69daf5010d
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2021-02-02 20:19:24 -0500

    Update SDADocumentService.java

--- backend/src/main/java/com/search/sdasearch/service/SDADocumentService.java
+++ backend/src/main/java/com/search/sdasearch/service/SDADocumentService.java
@@ -6,7 +6,18 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+/**************************************/
+/*                                    */
+/*  Program: SDA-Search               */
+/*  Author: Paul Trudel               */
+/*                                    */
+/*  Interface defining the methods    */
+/*  available from the document       */
+/*  service                           */
+/*                                    */
+/**************************************/
+
 public interface SDADocumentService {
 
-    public Page<SDADocument> searchDocuments(String query, Pageable pageable);
+    public Page<SDADocument> searchDocuments(String query, Pageable pageable); // Query the document index with user provided string
 }
